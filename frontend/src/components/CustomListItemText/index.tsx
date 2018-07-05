dc11e0c2cae50e47b3f2c542300d8bf70e59b206
Sam Lee <chunghinlee@umass.edu>
2018-07-05 14:35:28 -0400

    Minor change on listItemText

--- frontend/src/components/CustomListItemText/index.tsx
+++ frontend/src/components/CustomListItemText/index.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 
-type Props = { text: string, className: string };
+type Props = { text: string, className: string, styleBody?: boolean };
 
-const CustomListItemText: React.StatelessComponent<Props> = ({ text, className }) => (
+const CustomListItemText: React.StatelessComponent<Props> = ({
+     text, className, styleBody = false
+    }) => (
     <ListItemText
         disableTypography
         primary={
-            <Typography variant="subheading" className={className}>
+            <Typography variant={styleBody ? 'body1' : 'subheading'} className={className}>
                 {text}
             </Typography>}
         classes={{ root: className }}
