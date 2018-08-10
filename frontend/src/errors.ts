8fd96e95f905dac6634063c2ce370e6224c52654
Arjun Guha <arjun@cs.umass.edu>
2018-08-10 14:54:14 -0400

    Errors

---
+++ frontend/src/errors.ts
@@ -0,0 +1,36 @@
+import { USERNAME } from './store/userLogin/saga'
+import { EJSVERSION } from 'elementary-js/dist/version';
+import { OCELOTVERSION } from './version';
+
+function traceError(message: { [key: string]: any}) {
+    const err = {
+        username: USERNAME,
+        ejsversion: EJSVERSION,
+        ocelotversion: OCELOTVERSION,
+        ...message
+    };
+    fetch('https://us-central1-arjunguha-research-group.cloudfunctions.net/paws/error', {
+        method: 'POST',
+        body: JSON.stringify(err),
+        headers: { 'Content-Type': 'application/json' }
+    }).catch(reason => {
+        console.error('Failed to log error ', reason);
+    });
+    console.error(message);
+}
+
+window.addEventListener('error', (errorEvent) => {
+    traceError({
+        message: errorEvent.message,
+        line: errorEvent.lineno,
+        column: errorEvent.colno
+    });
+});
+
+const tracingConsole = {
+    error: function(message: string) {
+        traceError({ message: message });
+    }
+}
+
+export { tracingConsole as console };
