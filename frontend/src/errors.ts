2fd60deeeb81016108709234faafcee20b478558
Sam Lee <chunghinlee@umass.edu>
2018-08-11 18:10:23 -0400

    moved online detection to index.tsx

--- frontend/src/errors.ts
+++ frontend/src/errors.ts
@@ -27,17 +27,6 @@ window.addEventListener('error', (errorEvent) => {
     });
 });
 
-const updateOnlineStatus = (event: Event) => {
-    let condition = navigator.onLine ? "online" : "offline";
-    if (condition === 'online') {
-        throw new Error('Reconnected back to internet');
-    }
-    // do some stuff when it goes offline, disable the whole editor..etc
-}
-
-window.addEventListener('online',  updateOnlineStatus);
-window.addEventListener('offline', updateOnlineStatus);
-
 const tracingConsole = {
     error: function(message: string) {
         traceError({ message: message });
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,3 +12,14 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
+
+const updateOnlineStatus = (event: Event) => {
+    let condition = navigator.onLine ? "online" : "offline";
+    if (condition === 'online') {
+        throw new Error('Reconnected back to internet');
+    }
+    // do some stuff when it goes offline, disable the whole editor..etc
+}
+
+window.addEventListener('online',  updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);
