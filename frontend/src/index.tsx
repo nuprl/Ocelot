a5185a63d494f13e2ebfba8acad8e5a48db3b207
Sam Lee <chunghinlee@umass.edu>
2018-08-11 18:00:07 -0400

    add online/offline error reporting, removed duplicated error listener

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,10 +4,6 @@ import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import Index from './mainPage';
 
-import { USERNAME } from './store/userLogin/saga'
-import { EJSVERSION } from 'elementary-js/dist/version';
-import { OCELOTVERSION } from './version';
-
 const store = configureStore();
 
 ReactDOM.render(
@@ -16,24 +12,3 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
-
-
-window.addEventListener('error', (x) => {
-    const err = {
-        username: USERNAME,
-        ejsversion: EJSVERSION,
-        ocelotversion: OCELOTVERSION,
-        message: x.message,
-        line: x.lineno,
-        col: x.colno,
-        error: String(x.error)
-    };
-    fetch('https://us-central1-arjunguha-research-group.cloudfunctions.net/paws/error', {
-        method: 'POST',
-        body: JSON.stringify(err),
-        headers: { 'Content-Type': 'application/json' }
-    }).catch(reason => {
-        console.error('Failed to log error ', reason);
-    });
-    console.error(x.message);
-});
