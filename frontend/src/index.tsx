6f78cc6822e80f73bbc3adedefa522ceb97174ad
Sam Lee <chunghinlee@umass.edu>
2018-08-16 09:29:29 -0400

    updated check online

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,13 +7,17 @@ ReactDOM.render(
     document.querySelector('#root')
 );
 
-const updateOnlineStatus = (event: Event) => {
-    let condition = navigator.onLine ? "online" : "offline";
-    if (condition === 'online') {
-        throw new Error('Reconnected back to internet');
+window.addEventListener('load', () => {
+    const updateOnlineStatus = (event: Event) => {
+        let condition = navigator.onLine ? "online" : "offline";
+        if (condition === 'online') {
+            throw new Error('Reconnected back to internet');
+        }
+        // do some stuff when it goes offline, disable the whole editor..etc
     }
-    // do some stuff when it goes offline, disable the whole editor..etc
-}
-
-window.addEventListener('online',  updateOnlineStatus);
-window.addEventListener('offline', updateOnlineStatus);
+    
+    setTimeout(() => {
+        window.addEventListener('online',  updateOnlineStatus);
+        window.addEventListener('offline', updateOnlineStatus);
+    }, 5000);
+});
