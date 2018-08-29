be7d99a3dfb0ff383e63d742b2c7f5518ec87b24
Arjun Guha <arjun@cs.umass.edu>
2018-08-29 11:01:05 -0400

    File creation notification

--- frontend/src/SideDrawer.tsx
+++ frontend/src/SideDrawer.tsx
@@ -76,19 +76,21 @@ const NewFileField = ListItemStyles(class extends React.Component<NewFileFieldPr
 
             this.props.deleteFileField();
             this.setState({ newFileErrorMsg: '' });
+            state.notify('Creating new file ...');
             saveChanges({ fileName: name, type: 'create', changes: '' })
                 .then(result => {
                     if (result.status !== 'SUCCESS') {
                         throw 'failure response from server';
                     }
-                    state.files.next([name, ...state.files.getValue()]);
+                    state.files.next([...state.files.getValue(), name]);
                     this.props.selectedFilename.next(name);
+                    state.loadProgram.next({kind: 'program', name: name, content: '' });
+                    state.notify('File created.');
                 })
                 .catch(reason => {
                     state.notify('Could not create file');
                     console.error(`failed to create file (reason: ${reason}`);
                 });
-            state.loadProgram.next({kind: 'program', name: name, content: '' });
         };
     }
 
