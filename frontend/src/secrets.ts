bded5009dcf3959088bfcc051545b93eea6e828b
Sam Lee <chunghinlee@umass.edu>
2019-02-11 16:34:17 -0500

    More linting on merged files

--- frontend/src/secrets.ts
+++ frontend/src/secrets.ts
@@ -2,11 +2,10 @@
 export function getCloudFunctionBaseUrl(): string {
     if (window.location.hostname === 'www.ocelot-ide.org') {
         return 'https://us-central1-arjunguha-research-group.cloudfunctions.net/paws/';
-    }
-    else {
+    } else {
         return 'https://us-central1-arjunguha-research-group.cloudfunctions.net/ocelot-beta/';
     }
 }
 
-export const LOGIN_CLIENT_ID : string = '692270598994-p92ku4bbjkvcddouh578eb1a07s8mghc.apps.googleusercontent.com';
-export const MODULE_WL_URL : string = 'https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries.json';
+export const LOGIN_CLIENT_ID: string = '692270598994-p92ku4bbjkvcddouh578eb1a07s8mghc.apps.googleusercontent.com';
+export const MODULE_WL_URL: string = 'https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries.json';
