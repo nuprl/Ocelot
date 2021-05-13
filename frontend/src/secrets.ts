dc6f893b476b9a9f4b4b625ae0e925d24fdd39a2
Arjun Guha <a.guha@northeastern.edu>
2021-05-13 10:52:40 -0400

    Not so secret secrets

--- frontend/src/secrets.ts
+++ frontend/src/secrets.ts
@@ -1,4 +1,6 @@
-const isProd: boolean = window.location.hostname === 'www.ocelot-ide.org';
-export const CLD_FN_BASE_URL: string = `https://us-central1-arjunguha-research-group.cloudfunctions.net/ocelot/`,
-             LOGIN_CLIENT_ID: string = '692270598994-p92ku4bbjkvcddouh578eb1a07s8mghc.apps.googleusercontent.com',
-             MODULE_WL_URL: string = `https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries${isProd ? '' : '-beta'}.json`;
+const isProd = true;
+// The URL to the Cloud Function on the backend
+export const CLD_FN_BASE_URL = `https://us-central1-ocelot-ide-org.cloudfunctions.net/ocelot/`;
+// From backend/env.yaml
+export const LOGIN_CLIENT_ID = '842833214550-7apj11arna9fauh5tba8hl3bnej3dhqc.apps.googleusercontent.com';
+export const MODULE_WL_URL = `https://raw.githubusercontent.com/umass-compsci220/ocelot-settings/master/libraries${isProd ? '' : '-beta'}.json`;
